5c3d038fe2dc28813b4f558c045405460025f5c7
Akash Kumar <90201774+Akash-Kumar678@users.noreply.github.com>
2022-07-25 22:47:48 +0530

    env file added

--- controller/newsletterController.js
+++ controller/newsletterController.js
@@ -1,13 +1,14 @@
+require('dotenv').config()
 const request = require('request')
 
 function NewsController(req,res){
 
     var email=req.params.email;
   request.post({
-    url: 'https://us10.api.mailchimp.com/3.0/lists/905c3ce828/members',
+    url: `https://${process.env.DC}.api.mailchimp.com/3.0/lists/${process.env.LIST}/members`,
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Basic 789b85eab01e48f34f151605d5d0f390-us10'
+      'Authorization': `Basic ${process.env.API_KEY}`
     },
     form: JSON.stringify({
       email_address: email,
